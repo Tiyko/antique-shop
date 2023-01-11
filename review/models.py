6fd5e31cc3c403445aab6b4755e0e6ae3b9cb01e
Tiyko <johnny86shepherd@gmail.com>
2023-01-11 00:06:38 +0000

    add required to form fields

--- review/models.py
+++ review/models.py
@@ -5,15 +5,15 @@ from products.models import Product
 
 class Review(models.Model):
     """ Comment Review model """
-    title = models.CharField(max_length=150, blank=True, null=True)
-    content = models.CharField(max_length=500, blank=True, null=True)
+    title = models.CharField(max_length=150, blank=False, null=True)
+    content = models.CharField(max_length=500, blank=False, null=True)
     product_reviewed = models.ForeignKey(
         Product, related_name='product_reviewed', on_delete=models.CASCADE
         )
     user_reviewing = models.ForeignKey(
         User, related_name='user_reviewing', on_delete=models.CASCADE
         )
-    rating = models.IntegerField()
+    rating = models.IntegerField(blank=False)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
